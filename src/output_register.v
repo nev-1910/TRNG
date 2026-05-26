19e71c509113f741a08f160ea30e50b80f4ca8d1
nev-1910 <nevinphilip10@gmail.com>
2026-05-26 14:30:09 +0530

    Fixed output register reset logic

--- src/output_register.v
+++ src/output_register.v
@@ -2,26 +2,33 @@
 
 module output_register (
     input wire clk,
-    input wire random_bit,
+    input wire rst_n,
+    input wire bit_in,
     output reg [31:0] random_data,
     output reg ready
 );
 
-reg [5:0] count = 0;
+reg [5:0] count;
 
-always @(posedge clk)
-begin
-    random_data <= {random_data[30:0], random_bit};
-
-    if(count < 32)
-    begin
-        count <= count + 1;
-        ready <= 1'b0;
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        random_data <= 32'b0;
+        count <= 0;
+        ready <= 0;
     end
+    else begin
 
-    else
-    begin
-        ready <= 1'b1;
+        random_data <= {random_data[30:0], bit_in};
+
+        if (count == 31) begin
+            ready <= 1;
+            count <= 0;
+        end
+        else begin
+            ready <= 0;
+            count <= count + 1;
+        end
+
     end
 end
 
